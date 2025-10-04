3b9f51b6722d4c46408fa6f8257fd8f3a75d9f89
Sebastián Velasco <seguvega@hotmail.com>
2025-10-04 19:24:47 -0400

    Using useEffect from react
    Adapting the body dynamically

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,10 +1,23 @@
 import { BrowserRouter as Router} from 'react-router-dom'
+import { useEffect } from "react"; //To use effect
 import './App.css'
 import MainRouter from './MainRouter'
 
 function App() {
-  const navbar = document.querySelector('.navbar');
-  document.body.style.paddingTop = navbar.offsetHeight + "px";
+useEffect(() => {
+    const navbar = document.querySelector(".navbar");
+    if (navbar) {
+      document.body.style.paddingTop = navbar.offsetHeight + "px";
+
+      // update on window resize too
+      const handleResize = () => {
+        document.body.style.paddingTop = navbar.offsetHeight + "px";
+      };
+      window.addEventListener("resize", handleResize);
+
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
   return (
     <>
     <Router>
