a8d164ac4910ba452c58deee3314aff1411c7c94
Sebastián Velasco <seguvega@hotmail.com>
2025-10-04 18:57:29 -0400

    .css Corrections

--- portafolio/src/MainRouter.jsx
+++ portafolio/src/MainRouter.jsx
@@ -11,7 +11,7 @@ const MainRouter = () =>
 {
     return(
         <>
-        <div>
+        <div classname= "body">
             <Layout />
             <Routes>
                 <Route path="/" element ={<Home />}/>
